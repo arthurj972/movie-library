b22ceacc8180c80275787a63e4c5f34efdfeeae2
Arthur JANSSENS <j.arth@hotmail.fr>
2023-08-25 16:44:02 +0200

    app: use material ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,17 +6,20 @@ import Navbar from './components/Navbar';
 import NoMatch from './pages/errors/404';
 import Movie from './pages/movie';
 import Home from './pages/home';
+import { Container } from '@mui/material';
 
 const App = (): ReactElement => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/my' element={<MyLibrary />} />
-        <Route path='/movie/:id' element={<Movie />} />
-        <Route path='*' element={<NoMatch />} />
-      </Routes>
+      <Container fixed>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/my' element={<MyLibrary />} />
+          <Route path='/movie/:id' element={<Movie />} />
+          <Route path='*' element={<NoMatch />} />
+        </Routes>
+      </Container>
     </Router>
   );
 };
