f7cd5378cecf1976fd0a683cdbcd50ca97b2eeff
Arthur JANSSENS <j.arth@hotmail.fr>
2023-08-26 19:04:48 +0200

    app: add on your library integration

--- app/src/pages/movie.tsx
+++ app/src/pages/movie.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type ReactElement, useEffect } from 'react';
+import React, { useState, type ReactElement, useEffect, type SyntheticEvent } from 'react';
 import { useParams } from 'react-router-dom';
 import env from 'react-dotenv';
 
@@ -7,20 +7,31 @@ import { type IMovieDetails, type IMovieDetailsResult } from '../models/movie.mo
 import ErrorMessage from '../components/ErrorMessage';
 import MovieHoverRating from '../components/MovieHoverRating';
 
-import { Button, Chip, Tooltip } from '@mui/material';
+import { Button, Checkbox, Chip, Tooltip } from '@mui/material';
 
+// icons
 import TodayIcon from '@mui/icons-material/Today';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import LanguageIcon from '@mui/icons-material/Language';
 import RedeemIcon from '@mui/icons-material/Redeem';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const Movie = (): ReactElement => {
   const { id } = useParams();
 
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [movie, setMovie] = useState<IMovieDetails | null>(null);
+  const [updateOnLibraryLoading, setUpdateOnLibraryLoading] = useState<boolean>(false);
 
+  /**
+   * Get movie details
+   * @param {string} id
+   * @returns {Promise<IMovieDetailsResult>}
+   */
   const getMovieDetailsApi = async (
     id: string
   ): Promise<IMovieDetailsResult> => {
@@ -35,6 +46,51 @@ const Movie = (): ReactElement => {
       }
       return await response.json();
     });
+  };
+
+  /**
+   * Add or remove a movie on user library
+   * @param {boolean} add choose your method
+   * @returns {Promise}
+   */
+  const addOrRemoveToUserLibrary = async (add: boolean = true): Promise<void> => {
+    return await fetch(`${env.API_URL}/my-library`, {
+      method: add ? 'POST' : 'DELETE',
+      body: JSON.stringify({
+        moviedb_id: id
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    }).then(async (response) => {
+      if (!response.ok) {
+        throw new Error(await response.text());
+      }
+      return await response.json();
+    });
+  };
+
+  const onSavedOnLibraryChange = (
+    event: SyntheticEvent,
+    checked: boolean
+  ): void => {
+    setUpdateOnLibraryLoading(true);
+
+    // if check = add on library
+    // id no check = remove on library
+    addOrRemoveToUserLibrary(checked)
+      .then((data) => {
+        setErrorMessage(null);
+        setUpdateOnLibraryLoading(false);
+        if (movie != null) {
+          movie.on_my_library = checked;
+        }
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        setUpdateOnLibraryLoading(false);
+      });
   };
 
   useEffect(() => {
@@ -96,19 +152,28 @@ const Movie = (): ReactElement => {
           <Tooltip title={`${movie.vote_count} votes`}>
             <Chip icon={<ThumbUpIcon />} label={`${movie.vote_average}/10`} variant="outlined" sx={{ m: 0.5 }} />
           </Tooltip>
+
+          <Tooltip title={`${movie.on_my_library ? 'Uns' : 'S'}ave on your library ?`}>
+            <Checkbox
+              {...label}
+              disabled={updateOnLibraryLoading}
+              checked={movie.on_my_library}
+              onChange={onSavedOnLibraryChange}
+              icon={<BookmarkBorderIcon />}
+              checkedIcon={<BookmarkIcon />}
+            />
+          </Tooltip>
         </div>
 
         <p style={{ marginBottom: 50 }}>{movie.overview}</p>
 
         <MovieHoverRating />
 
-        <div>
-          {movie.homepage !== '' &&
-            <Button variant="contained" href={movie.homepage} target='_blank'>
-              <LanguageIcon />Go to homepage
-            </Button>
-          }
-        </div>
+        {movie.homepage !== '' &&
+          <Button variant="contained" href={movie.homepage} target='_blank'>
+            <LanguageIcon />Go to homepage
+          </Button>
+        }
       </>
     );
   } else {
