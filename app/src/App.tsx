e5305350e891070cac9e1bd477d3750c3c9968eb
Arthur JANSSENS <j.arth@hotmail.fr>
2023-08-26 11:47:47 +0200

    app: little pages refacto

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,10 @@
 import React, { type ReactElement } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MyLibrary from './pages/my-library';
-import Navbar from './components/Navbar';
-import NoMatch from './pages/errors/404';
-import Movie from './pages/movie';
-import Home from './pages/home';
 import { Container } from '@mui/material';
+
+import Navbar from './components/Navbar';
+import Pages from './pages';
 
 const App = (): ReactElement => {
   return (
@@ -14,10 +12,10 @@ const App = (): ReactElement => {
       <Navbar />
       <Container fixed>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/my' element={<MyLibrary />} />
-          <Route path='/movie/:id' element={<Movie />} />
-          <Route path='*' element={<NoMatch />} />
+          <Route path='/' element={<Pages.Home />} />
+          <Route path='/my' element={<Pages.MyLibrary />} />
+          <Route path='/movie/:id' element={<Pages.Movie />} />
+          <Route path='*' element={<Pages.NoMatch />} />
         </Routes>
       </Container>
     </Router>
---
+++ app/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import MyLibrary from './my-library';
+import NoMatch from './errors/404';
+import Movie from './movie';
+import Home from './home';
+
+export default {
+  MyLibrary,
+  NoMatch,
+  Movie,
+  Home
+}
