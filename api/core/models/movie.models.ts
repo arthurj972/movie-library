0fe9b183fc82c270a5f432faecaa84793f9cffbe
Arthur JANSSENS <j.arth@hotmail.fr>
2023-08-26 13:50:26 +0200

    api: movie details imporoved

--- api/core/models/movie.models.ts
+++ api/core/models/movie.models.ts
@@ -6,3 +6,27 @@ export interface IMovie {
     title: string
     on_my_library: boolean
 };
+
+export interface IMovieDetails {
+    backdrop_path: string
+    budget: number
+    budget_format: string
+    genres: Array<{
+        id: number
+        name: string
+    }>
+    homepage: string
+    moviedb_id: number
+    original_language: string
+    original_title: string
+    overview: string
+    poster_path: string
+    release_date: Date
+    revenue: number
+    revenue_format: string
+    status: string
+    title: string
+    vote_average: number
+    vote_count: number
+    on_my_library: boolean
+}
--- api/routes/movie/movie.controllers.ts
+++ api/routes/movie/movie.controllers.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 import { type NextFunction, type Request, type Response } from 'express';
-import { type IMovie } from '../../core/models/movie.models';
+import { nFormatter } from '../../core/functions';
+import { type IMovie, type IMovieDetails } from '../../core/models/movie.models';
 import { type IMovieDBdetailsResult, type IMovieDBresult } from '../../core/models/moviedb.models';
 import { type IMovieParams, type ISearchQuery } from './movie.schemas';
 
@@ -50,8 +51,30 @@ const movie = async (req: Request, res: Response, next: NextFunction): Promise<v
         });
         const movie: IMovieDBdetailsResult = response.data;
 
+        // clear data (removes unnecessary variables & adds new)
+        const finalMovie: IMovieDetails = {
+            backdrop_path: movie.backdrop_path,
+            budget: movie.budget,
+            budget_format: nFormatter(movie.budget, 2),
+            genres: movie.genres,
+            homepage: movie.homepage,
+            moviedb_id: movie.id,
+            original_language: movie.original_language,
+            original_title: movie.original_title,
+            overview: movie.overview,
+            poster_path: movie.poster_path,
+            release_date: movie.release_date,
+            revenue: movie.revenue,
+            revenue_format: nFormatter(movie.revenue, 2),
+            status: movie.status,
+            title: movie.title,
+            vote_average: movie.vote_average,
+            vote_count: movie.vote_count,
+            on_my_library: false
+        }
+
         res.json({
-            movie
+            movie: finalMovie
         });
     } catch (error) {
         next(error);
